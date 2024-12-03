f49c85fa953b8cb35c663fa1e8d831666b9ed9f7
JaswithaG <gummijaswitha@gmail.com>
2024-12-03 12:04:45 +0530

    Add files via upload

---
+++ assingmentoperators.java
@@ -0,0 +1,8 @@
+public class assingmentoperators{
+    public static void main(String args[]){
+        int a=45;
+        System.out.println(a+=10);
+        System.out.println(a-=10);
+        System.out.println(a*=2);
+    }
+}
