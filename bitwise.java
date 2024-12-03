4b89f9e1ec33f9e337be12766be730faae8705b3
JaswithaG <gummijaswitha@gmail.com>
2024-12-03 14:10:03 +0530

    Add files via upload

---
+++ bitwise.java
@@ -0,0 +1,9 @@
+public class bitwise{
+    public static void main(String args[]){
+        System.out.println(15^12);
+        System.out.println(15|12);
+        System.out.println(15&12);
+        System.out.println(110^110);
+        System.out.println(140^150);
+    }
+}
