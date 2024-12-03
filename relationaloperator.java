891376a57cef0c2df7df58b8fcb90d507beaafbf
JaswithaG <gummijaswitha@gmail.com>
2024-12-03 10:27:17 +0530

    Add files via upload

---
+++ relationaloperator.java
@@ -0,0 +1,13 @@
+public class relationaloperator{
+    public static void main(String args[]){
+        int a=18;
+        int b=73;
+        System.out.println(a==b);
+        System.out.println(a!=b);
+        System.out.println(a>b);
+        System.out.println(a<b);
+        System.out.println(a<b);
+        System.out.println(a<=b);
+        System.out.println(a>=b);
+    }
+} 
